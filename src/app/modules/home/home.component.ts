ed754a6ad24592294a62afd29c7f53b3f3ee2d79
Caio Satel <caio_satel@hotmail.com>
2025-02-02 16:43:28 -0300

    feat: implementando formGroup e formControl para receber dados do formulário

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
+
+  signUpForm = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  });
+
+  constructor (private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm() {
+    console.log(this.loginForm.value);
+  }
+
+  onSubmitSignUpForm() {
+    console.log(this.signUpForm.value);
+  }
 }
