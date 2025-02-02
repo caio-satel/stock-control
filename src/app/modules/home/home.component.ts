2ec6891dd2990e3e11fe85fe364c1ded7acbc373
Caio Satel <caio_satel@hotmail.com>
2025-02-02 18:46:43 -0300

    feat: implementanto funcionalidade de criar e logar usuario

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,9 @@
+import { CookieService } from 'ngx-cookie-service';
+import { AuthUserRequest } from './../../../../../API/stock-api/src/models/interfaces/user/AuthUserRequest';
+import { UserService } from './../../services/users/user.service';
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { SignupUserRequest } from 'src/app/models/interfaces/user/signupUserRequest';
 
 @Component({
   selector: 'app-home',
@@ -20,13 +24,42 @@ export class HomeComponent {
     password: ['', [Validators.required]]
   });
 
-  constructor (private formBuilder: FormBuilder) {}
+  constructor (private formBuilder: FormBuilder, private UserService: UserService, private CookieService: CookieService) { }
 
   onSubmitLoginForm() {
-    console.log(this.loginForm.value);
+    if (this.loginForm.value && this.loginForm.valid) {
+      this.UserService.authUser(this.loginForm.value as AuthUserRequest)
+      .subscribe({
+        next: (response) => {
+          if (response) {
+            alert('Usuario logado com sucesso!');
+            this.CookieService.set('USER_INFO', response?.token);
+            this.loginForm.reset();
+            this.loginCard = true;
+          }
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      })
+      };
   }
 
   onSubmitSignUpForm() {
-    console.log(this.signUpForm.value);
-  }
+    if (this.signUpForm.value && this.signUpForm.valid) {
+      this.UserService.signUpUser(this.signUpForm.value as SignupUserRequest)
+      .subscribe({
+        next: (response) => {
+          if (response) {
+            alert('Usuario registrado com sucesso!');
+            this.signUpForm.reset();
+            this.loginCard = true;
+          }
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      })
+      };
+    }
 }
